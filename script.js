22da461968bfb8529dea525e69b4d8bdb7bd80af
Bharat <hibharatj@gmail.com>
2025-06-22 18:51:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll(".code");
+
+inputs[0].focus();
+
+inputs.forEach((input, index) => {
+  input.addEventListener("keydown", (e) => {
+    if (e.key >= "0" && e.key <= "9") {
+      input.value = "";
+      setTimeout(() => {
+        if (index < inputs.length - 1) {
+          inputs[index + 1].focus();
+        }
+      }, 10);
+    } else if (e.key === "Backspace") {
+      input.value = "";
+      if (index > 0) {
+        inputs[index - 1].focus();
+      }
+    }
+  });
+
+  input.addEventListener("input", () => {
+    // Prevent non-numeric input
+    input.value = input.value.replace(/[^0-9]/g, "");
+  });
+});
